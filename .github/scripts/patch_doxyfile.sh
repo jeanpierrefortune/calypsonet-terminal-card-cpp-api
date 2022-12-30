c5bc03c1af1c8c82a2400633e868d72d21148322
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2022-12-30 16:19:15 +0100

    ci: fix doc publication process

--- .github/scripts/patch_doxyfile.sh
+++ .github/scripts/patch_doxyfile.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 echo "Compute the current API version..."
-version=""
-suffix=$1
+version=$1
 
 if [ "$version" = "" ]; then
   # SNAPSHOT version
   version="$(sed -rn 's/.*MAJOR.*\"(.*)\".*/\1/p' CMakeLists.txt).$(sed -rn 's/.*MINOR.*\"(.*)\".*/\1/p' CMakeLists.txt).$(sed -rn 's/.*PATCH.*\"(.*)\".*/\1/p' CMakeLists.txt)-SNAPSHOT"
 fi
 
-echo "Computed current API version: $version$suffix"
+echo "Computed current API version: $version"
 
-sed -i "s/%PROJECT_VERSION%/$version$suffix/g" ./.github/doxygen/Doxyfile
+sed -i "s/%PROJECT_VERSION%/$version/g" ./.github/doxygen/Doxyfile
