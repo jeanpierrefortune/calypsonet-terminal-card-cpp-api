47c5625fc87d755f5e948b2d901d348b241e088d
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2022-12-30 15:53:40 +0100

    ci: distinguish release and snapshot doc publication

--- .github/scripts/patch_doxyfile.sh
+++ .github/scripts/patch_doxyfile.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 echo "Compute the current API version..."
-version=$1
+version=""
+suffix=$1
 
 if [ "$version" = "" ]; then
   # SNAPSHOT version
   version="$(sed -rn 's/.*MAJOR.*\"(.*)\".*/\1/p' CMakeLists.txt).$(sed -rn 's/.*MINOR.*\"(.*)\".*/\1/p' CMakeLists.txt).$(sed -rn 's/.*PATCH.*\"(.*)\".*/\1/p' CMakeLists.txt)-SNAPSHOT"
 fi
 
-echo "Computed current API version: $version"
+echo "Computed current API version: $version$suffix"
 
-sed -i "s/%PROJECT_VERSION%/$version/g" ./.github/doxygen/Doxyfile
+sed -i "s/%PROJECT_VERSION%/$version$suffix/g" ./.github/doxygen/Doxyfile
